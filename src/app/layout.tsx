b1d07bd41a4dd5984ffaa5adcbb16c1323047d72
한병현 <gksqudgus777@naver.com>
2026-04-20 14:26:42 +0900

    chore: tailwind.config.ts 생성 및 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import "../styles/globals.css";
-import "../styles/reset.css";
 
 export default function RootLayout({
   children,
@@ -8,6 +7,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
