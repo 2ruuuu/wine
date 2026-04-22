70e586bf881cbe9b14b6b72a15c0065894bcd4e9
leeeunji <ayozz6742@naver.com>
2026-04-22 19:59:54 +0900

    feat:헤더 시멘틱변경

--- src/components/gnb/Header.tsx
+++ src/components/gnb/Header.tsx
@@ -6,7 +6,7 @@ import { HeaderProps } from './type';
 
 const Header = ({ isLogIn }: HeaderProps) => {
   return (
-    <div className="max-w-[1140px] md:h-[70px] h-[50px] bg-black md:px-[60px] px-[20px] text-white flex justify-between items-center rounded-[4px] mx-auto">
+    <header className="max-w-[1140px] md:h-[70px] h-[50px] bg-black md:px-[60px] px-[20px] text-white flex justify-between items-center rounded-[4px] mx-auto">
       <h1>
         <Link href="/">
           <Image src={Logo} alt="" />
@@ -15,15 +15,15 @@ const Header = ({ isLogIn }: HeaderProps) => {
       {isLogIn ? (
         <button className=" w-[45px] h-[45px] rounded-full overflow-hidden border-1 border-white">
           <img
-            src="https://cdn.jk-daily.co.kr/news/photo/202602/37252_29268_3439.jpg"
-            alt=""
+            src="https://i.namu.wiki/i/QMYkLdvhM3sxErXFfp6f8OooYMjrwqo7nraefN3QIGNMjAtPY3NcLS5ubG4KD70N8AOyPxTQy_-MSsrtxgJhIg.webp"
+            alt="프로필 이미지"
             className="w-full h-full object-cover"
           />
         </button>
       ) : (
         <button className="md:text-[16px] text-[12px]">로그인</button>
       )}
-    </div>
+    </header>
   );
 };
 
