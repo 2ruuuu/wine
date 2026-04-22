9c8760f0bcf4d2f13504bd1667e5311988068f1f
choi-il-woo <chldlfdn0440@naver.com>
2026-04-22 09:41:28 +0900

    chore: 아이콘 저장

---
+++ src/constants/icons.ts
@@ -0,0 +1,45 @@
+import Star from '@/assets/icons/star.svg';
+import Apple from '@/assets/icons/apple.svg';
+import Baking from '@/assets/icons/baking.svg';
+import Berry from '@/assets/icons/berry.svg';
+import Caramel from '@/assets/icons/caramel.svg';
+import Cherry from '@/assets/icons/cherry.svg';
+import Chocolate from '@/assets/icons/chocolate.svg';
+import Citrus from '@/assets/icons/citrus.svg';
+import Coconut from '@/assets/icons/coconut.svg';
+import Flower from '@/assets/icons/flower.svg';
+import Grass from '@/assets/icons/grass.svg';
+import Leather from '@/assets/icons/leather.svg';
+import Mineral from '@/assets/icons/mineral.svg';
+import Oak from '@/assets/icons/Oak.svg';
+import Peach from '@/assets/icons/peach.svg';
+import Pepper from '@/assets/icons/pepper.svg';
+import Soil from '@/assets/icons/soil.svg';
+import Spice from '@/assets/icons/spice.svg';
+import Tobacco from '@/assets/icons/tobacco.svg';
+import Tropical from '@/assets/icons/tropical.svg';
+import Vanilla from '@/assets/icons/vanilla.svg';
+
+export {
+  Star,
+  Apple,
+  Baking,
+  Berry,
+  Caramel,
+  Cherry,
+  Chocolate,
+  Citrus,
+  Coconut,
+  Flower,
+  Grass,
+  Leather,
+  Mineral,
+  Oak,
+  Peach,
+  Pepper,
+  Soil,
+  Spice,
+  Tobacco,
+  Tropical,
+  Vanilla,
+};
