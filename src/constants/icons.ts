319d39f7c623f761da41b92d7e2edd4a8a0d2d66
kujihye <gji135@naver.com>
2026-04-24 01:27:33 +0900

    공통 Button 컴포넌트 구현

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -4,5 +4,18 @@ import Search from '@/assets/icons/search.svg';
 import Close from '@/assets/icons/close.svg';
 import CameraBlack from '@/assets/icons/camera-black.svg';
 import CameraGray from '@/assets/icons/camera-gray.svg';
+import Kakao from '@/assets/icons/kakao.svg';
+import ArrowLeft from '@/assets/icons/arrow-left.svg';
+import ArrowRight from '@/assets/icons/arrow-right.svg';
 
-export { Alert, Search, Star, Close, CameraBlack, CameraGray };
+export {
+  Alert,
+  Search,
+  Star,
+  Close,
+  CameraBlack,
+  CameraGray,
+  Kakao,
+  ArrowLeft,
+  ArrowRight,
+};
