46239fd63e4aef23055add7625350dbcfba48b86
최문경 <mkv9312101777@gmail.com>
2026-04-23 13:31:32 +0900

    fix: 레이아웃 초기 설정 복구

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,22 @@
+import ModalProvider from "@/components/modal/ModalProvider";
 import "../styles/globals.css";
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+        />
+      </head>
+      <body>
+        <ModalProvider>{children}</ModalProvider>
+      </body>
     </html>
   );
 }
