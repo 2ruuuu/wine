f3ae03715a5c333930a72384d8fb988f24180a95
한병현 <gksqudgus777@naver.com>
2026-04-27 23:25:59 +0900

    feat: wine list mock 데이터 추가

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -3,9 +3,21 @@ import wineTypeWhiteImage from '@/assets/images/wine-type-white.png';
 import wineTypeSparklingImage from '@/assets/images/wine-type-sparkling.png';
 import profileImage from '@/assets/images/profile.png';
 
+// 삭제 예정 이미지
+import wineMockImage1 from '@/assets/images/wine-mock-1.png';
+import wineMockImage2 from '@/assets/images/wine-mock-2.png';
+import wineMockImage3 from '@/assets/images/wine-mock-3.png';
+import wineMockImage4 from '@/assets/images/wine-mock-4.png';
+
 export {
   wineTypeRedImage,
   wineTypeWhiteImage,
   wineTypeSparklingImage,
   profileImage,
+
+  /** 삭제 예정 이미지 */
+  wineMockImage1,
+  wineMockImage2,
+  wineMockImage3,
+  wineMockImage4,
 };
---
+++ src/mocks/SuggestedWinesMockData.ts
@@ -0,0 +1,112 @@
+import { SuggestedWineProps } from '@/app/wines/type';
+import {
+  wineMockImage1,
+  wineMockImage2,
+  wineMockImage3,
+  wineMockImage4,
+} from '@/constants/images';
+
+export const suggestedWinesMockData: SuggestedWineProps[] = [
+  {
+    id: 1,
+    name: 'Château Margaux',
+    region: 'Bordeaux, France',
+    image: wineMockImage1.src,
+  },
+  {
+    id: 2,
+    name: 'Barolo Riserva',
+    region: 'Piedmont, Italy',
+    image: wineMockImage2.src,
+  },
+  {
+    id: 3,
+    name: 'Rioja Gran Reserva',
+    region: 'Rioja, Spain',
+    image: wineMockImage3.src,
+  },
+  {
+    id: 4,
+    name: 'Napa Valley Cabernet Sauvignon',
+    region: 'California, USA',
+    image: wineMockImage4.src,
+  },
+  {
+    id: 5,
+    name: 'Bourgogne Pinot Noir',
+    region: 'Burgundy, France',
+    image: wineMockImage4.src,
+  },
+  {
+    id: 6,
+    name: 'Chianti Classico',
+    region: 'Tuscany, Italy',
+    image: wineMockImage3.src,
+  },
+  {
+    id: 7,
+    name: 'Côtes du Rhône Rouge',
+    region: 'Rhône Valley, France',
+    image: wineMockImage2.src,
+  },
+  {
+    id: 8,
+    name: 'Malbec Reserva',
+    region: 'Mendoza, Argentina',
+    image: wineMockImage1.src,
+  },
+  {
+    id: 9,
+    name: 'Shiraz Signature',
+    region: 'Barossa Valley, Australia',
+    image: wineMockImage1.src,
+  },
+  {
+    id: 10,
+    name: 'Sauvignon Blanc',
+    region: 'Marlborough, New Zealand',
+    image: wineMockImage4.src,
+  },
+  {
+    id: 11,
+    name: 'Riesling Kabinett',
+    region: 'Mosel, Germany',
+    image: wineMockImage2.src,
+  },
+  {
+    id: 12,
+    name: 'Champagne Brut',
+    region: 'Champagne, France',
+    image: wineMockImage3.src,
+  },
+  {
+    id: 13,
+    name: 'Prosecco Extra Dry',
+    region: 'Veneto, Italy',
+    image: wineMockImage1.src,
+  },
+  {
+    id: 14,
+    name: 'Rosé de Provence',
+    region: 'Provence, France',
+    image: wineMockImage4.src,
+  },
+  {
+    id: 15,
+    name: 'Tempranillo Reserva',
+    region: 'Castilla-La Mancha, Spain',
+    image: wineMockImage2.src,
+  },
+  {
+    id: 16,
+    name: 'Cabernet Franc',
+    region: 'Loire Valley, France',
+    image: wineMockImage3.src,
+  },
+  {
+    id: 17,
+    name: 'Zinfandel Old Vine',
+    region: 'California, USA',
+    image: wineMockImage1.src,
+  },
+];
