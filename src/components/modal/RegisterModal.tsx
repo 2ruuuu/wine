f18376f67a6460e28d007f8c4de3cdd97a485af0
leeeunji <ayozz6742@naver.com>
2026-04-28 14:29:13 +0900

    feat/modal 수정

--- src/components/modal/RegisterModal.tsx
+++ src/components/modal/RegisterModal.tsx
@@ -1,18 +1,5 @@
-import PhotoInput from '../Input/PhotoInput';
-import TextInput from '../Input/TextInput';
-
 const NickNameModal = () => {
-  return (
-    <div>
-      {/* <PhotoInput label="와인 사진" register={'winePhoto1'} /> */}
-      <TextInput
-        label="제목"
-        type="text"
-        placeholder="내용을 입력해주세요"
-        register={register('title1')}
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default NickNameModal;
--- src/components/modal/type.ts
+++ src/components/modal/type.ts
@@ -13,7 +13,7 @@ export type ModalProviderProps =
   | { type: 'register' }
   | { type: 'review' }
   | { type: 'delete' }
-  | { type: 'nickname'; name: 'string' }
+  | { type: 'nickname'; name: string }
   | null;
 
 export type ModalContextProps = {
