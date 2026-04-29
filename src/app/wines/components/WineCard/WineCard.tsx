552694c68b2cd25bd42ef29721186d64008d61ea
한병현 <gksqudgus777@naver.com>
2026-04-29 19:42:40 +0900

    feat: Wine Card 컴포넌트 구현

---
+++ src/app/wines/components/WineCard/WineCard.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import { WineCardProps } from '@/app/wines/type';
+import StarRating from '@/components/StarRating/StarRating';
+
+const WineCard = ({ 
+  name,
+  image,
+  avgRating,
+  reviewCount,
+  recentReview,
+ }: WineCardProps) => {
+  return (
+    <div className="flex flex-col gap-6 max-w-[370px] w-full">
+      <div className="flex justify-center items-center w-[370px] h-[360px] bg-[#F2F2F2]">
+        <Image
+          src={image}
+          alt={name}
+          height={288}
+          width={75}
+          className="h-72 w-auto"
+        />
+      </div>
+      <div className="flex flex-col gap-3 divide-y divide-solid divide-[#D1D1D1]">
+        <div className="flex flex-col gap-3 pb-[25px]">
+          <div className="flex gap-3">
+            <StarRating rating={avgRating} />
+            <span className="text-body-sm text-[#A3A3A3]">{reviewCount}개의 후기</span>
+          </div>
+          <h2 className="text-heading-lg font-semibold">{name}</h2>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-body-md font-medium">최신 후기</h3>
+          <p className="text-body-sm text-[#A3A3A3] line-clamp-2">
+            {recentReview.content}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WineCard;
