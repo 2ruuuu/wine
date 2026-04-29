10711d4862b7feb9197f8abc5ebd98453412cd2b
choi-il-woo <chldlfdn0440@naver.com>
2026-04-30 08:45:21 +0900

    feat: 맛 컴포넌트 제작 완료

--- src/app/wines/[id]/components/Flavor/Flavor.tsx
+++ src/app/wines/[id]/components/Flavor/Flavor.tsx
@@ -1,9 +1,34 @@
+import Taste from '@/components/Taste/Taste';
+import Mock from '@/mocks/wineDetail.json';
+
 const Flavor = () => {
   return (
-    <div>
-      <div>
-        <span>어떤 맛이 나나요?</span>
-        <span></span>
+    <div className="flex flex-col gap-8 w-[500px]">
+      <div className="flex justify-between items-center">
+        <span className="text-2xl font-semibold">어떤 맛이 나나요?</span>
+        <span className="text-gray-300">({Mock.reviewCount}명 참여)</span>
+      </div>
+      <div className="flex flex-col gap-5">
+        <Taste
+          variant="label-boxed-long"
+          value={Mock.reviewStats.averageLightBold}
+          type="lightBold"
+        />
+        <Taste
+          variant="label-boxed-long"
+          value={Mock.reviewStats.averageSmoothTannic}
+          type="smoothTannic"
+        />
+        <Taste
+          variant="label-boxed-long"
+          value={Mock.reviewStats.averageDrySweet}
+          type="drySweet"
+        />
+        <Taste
+          variant="label-boxed-long"
+          value={Mock.reviewStats.averageSoftAcidic}
+          type="softAcidic"
+        />
       </div>
     </div>
   );
