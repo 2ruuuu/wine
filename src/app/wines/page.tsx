90a77f22f37319931026438a91f979664590108d
한병현 <gksqudgus777@naver.com>
2026-05-07 20:15:49 +0900

    design: 와인 페이지 상단 여백 조정

--- src/app/wines/page.tsx
+++ src/app/wines/page.tsx
@@ -4,7 +4,7 @@ import WinesResultsSection from './components/WinesResultsSection/WinesResultsSe
 const WinesPage = () => {
   return (
     <div className="mb-21">
-      <div className="bg-[url(@/assets/images/suggested-wine-background.png)] pt-[85px] md:pt-[127px] xl:pt-38.5 pb-7 md:pb-11 xl:pb-15 xl:rounded-b-[88px]">
+      <div className="bg-[url(@/assets/images/suggested-wine-background.png)] pt-[35px] md:pt-[77px] xl:pt-[104px] pb-7 md:pb-11 xl:pb-15 xl:rounded-b-[88px]">
         <SuggestedWineList />
       </div>
       <WinesResultsSection />
