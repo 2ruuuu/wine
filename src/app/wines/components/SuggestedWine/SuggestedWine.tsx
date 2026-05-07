541e10520cff130225a4d45f5eeeb250f858993d
한병현 <gksqudgus777@naver.com>
2026-05-08 01:51:18 +0900

    refactor: 추천 와인을 클릭하면 상세페이지 이동

--- src/app/wines/components/SuggestedWine/SuggestedWine.tsx
+++ src/app/wines/components/SuggestedWine/SuggestedWine.tsx
@@ -1,22 +1,25 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { SuggestedWineProps } from '@/app/wines/components/SuggestedWine/type';
 import { NoImage } from '@/constants/images';
 
-const SuggestedWine = ({ name, region, image }: SuggestedWineProps) => {
+const SuggestedWine = ({ id, name, region, image }: SuggestedWineProps) => {
   return (
-    <div className="flex flex-col justify-center items-center gap-3 w-[150px] h-[243px] xl:gap-4 xl:w-50 xl:h-80">
-      <Image
-        src={image ?? NoImage}
-        alt={`${name}-image`}
-        width={62}
-        height={228}
-        className="h-[165px] w-auto xl:h-57 xl:w-auto"
-      />
-      <div className="flex flex-col justify-center items-center gap-[6px] xl:gap-2 text-center">
-        <span className="text-sm xl:text-base font-normal text-[#31302F]">{name}</span>
-        <span className="text-caption xl:text-sm font-normal text-[#A3A3A3]">{region}</span>
+    <Link href={`/wines/${id}`} className="block">
+      <div className="flex flex-col justify-center items-center gap-3 w-[150px] h-[243px] xl:gap-4 xl:w-50 xl:h-80">
+        <Image
+          src={image ?? NoImage}
+          alt={`${name}-image`}
+          width={62}
+          height={228}
+          className="h-[165px] w-auto xl:h-57 xl:w-auto"
+        />
+        <div className="flex flex-col justify-center items-center gap-[6px] xl:gap-2 text-center">
+          <span className="text-sm xl:text-base font-normal text-[#31302F]">{name}</span>
+          <span className="text-caption xl:text-sm font-normal text-[#A3A3A3]">{region}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
